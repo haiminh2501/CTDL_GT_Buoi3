335893260e39342520e033d7b9e203650dcfc715
haiminh2501 <165640554+haiminh2501@users.noreply.github.com>
2024-07-24 11:17:49 +0700

    Add files via upload

---
+++ TinhTongMangDeQui.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTongMangDeQui
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = 5;
+            int sum = DivideAndConquerSum(1, n);
+            Console.WriteLine("Tong tu 1 den {n} la: {sum}");
+        }
+
+        static int DivideAndConquerSum(int start, int end)
+        {
+          
+            if (start == end)
+            {
+                return start;
+            }
+
+          
+            int mid = (start + end) / 2;
+
+            
+            int leftSum = DivideAndConquerSum(start, mid);
+            int rightSum = DivideAndConquerSum(mid + 1, end);
+
+            
+            return leftSum + rightSum;
+        }
+    }
+
+}
---
+++ TinhTongMangKhongDeQui.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTongMangKhongDeQui
+{
+       
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = 5;
+            int mid = n / 2;
+            int sum1 = SumRange(1, mid);
+            int sum2 = SumRange(mid + 1, n);
+            int totalSum = sum1 + sum2;
+
+            Console.WriteLine("Tong tu 1 den {n} la: {totalSum}");
+        }
+
+        static int SumRange(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+}
